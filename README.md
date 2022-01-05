d52eeae80ed26ad6439e9ddc3022e5ecae27b285
Timur <timur_kamalov_97@mail.ru>
2022-01-05 22:32:28 +0300

    Created  project

---
+++ README.md
@@ -0,0 +1,7 @@
+# ExcelApi
+
+I wrote this project thinking of automizing customer's work in Excel.
+However, he migrated to GoogleTables (Later I developed for GoogleTables)
+and this project remained untouched.
+
+Nevertheless, it seems to me useful.
---
+++ Work_with_Excel/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace Work_with_Excel
+{
+    class Excel
+    {
+        string path = "";
+        _Application excel = new Application();
+        Workbook workbook;
+        Worksheet worksheet;
+
+        public Excel(string path, int sheet)
+        {
+            this.path = path;
+            workbook = excel.Workbooks.Open(path);
+            worksheet = workbook.Worksheets[sheet];
+        }
+
+        public Excel(string path)
+        {
+            int sheet = 1;
+            this.path = path;
+            workbook = excel.Workbooks.Open(path);
+            worksheet = workbook.Worksheets[sheet];
+        }
+
+        public string ReadCell(int i, int j)
+        {
+            if (worksheet.Cells[i, j].Value2 != null)
+                return worksheet.Cells[i, j].Value2.ToString();
+            else return "Пусто :(";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            //Excel excel = new Excel("C:\\Users\\USER\\source\\repos\\Master\\Test.xlsx", 1);
+
+            //for (int i = 1; i < 10; i++)            
+            //    Console.WriteLine(excel.ReadCell(1, i));
+            
+            Console.ReadKey();
+
+        }
+    }
+}
